2595e45d219a171bbef083958c848e80bf2b64cf
9adam <lengyel.adam95@gmail.com>
2020-07-16 00:49:03 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/`)
+      .then((res) => {
+        this.setState({ posts: res.data })
+      }).catch((err) => console.error('Error occured during fetching artist details', err))
+  }
+
+  render() {
+    console.log('posts', this.state.posts)
+    return (
+      <div className="container">
+        <h1 class="main-title">Posts</h1>
+        {
+          this.state.posts.map(post => {
+            return <div className="post-wrapper">
+              <h4 className="post-title">{post.title}</h4>
+              <p className="post-content">{post.content}</p>
+            </div>
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
